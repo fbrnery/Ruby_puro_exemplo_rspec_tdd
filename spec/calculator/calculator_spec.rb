1eab519d4fcb9d24a9623d50022b49dbc5b858f3
Fabricio Miranda <fbrnery@gmail.com>
2023-01-03 22:00:31 -0300

    Configurei o projeto

---
+++ spec/calculator/calculator_spec.rb
@@ -0,0 +1,18 @@
+require 'calculator'
+
+describe Calculator do
+  context "use sum method for 2 numbers" do
+    it 'positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
+
+    it 'negative numbers' do
+    calc = Calculator.new
+    result = calc.sum(-5, 7)
+    expect(result).to eq(2)
+
+    end
+  end
+end
